a529164a0a075e0f5f3ab7af8e64dfc75b1c6093
Pedro Correia <helitonpedrosc@gmail.com>
2024-12-05 19:52:11 -0300

    Commit inicial

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container mt-4 text-body-secondary">
+    <div class="card" style="margin-bottom: 100px">
+      <h1 class="text-center">PokeVite</h1>
+      <div class="card-body">
+        <h2>Objetivo:</h2>
+        <p>Esse é mais um projeto desenvolvido para fins didáticos com intúito de praticar o desenvolvimento com Vue 3 utilizando Vite e consumindo a API PokeAPI.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
